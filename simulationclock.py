ae53eaf806c29e9c8c4e1b31ad499c759dca19f4
markzuccu175 <pranav.vajreshwari@iiitb.org>
2021-03-16 21:04:02 +0530

    Update simulationclock.py

--- simulationclock.py
+++ simulationclock.py
@@ -23,8 +23,8 @@ class Simulation:
         self.time_leaving_count1 = float('inf')  # time when a customer leaves counter 1,float('inf') denotes infinity,
         # initially a departure event is scheduled at infinity to ensure first event is arrival'''
         self.time_leaving_count2 = float('inf')  # time when a customer leaves counter 2
-        self.dep_sum1 = 0  # Sum of service times by teller 1
-        self.dep_sum2 = 0  # Sum of service times by teller 2
+        self.dep_sum_time1 = 0  # Sum of service times by teller 1
+        self.dep_sum_time2 = 0  # Sum of service times by teller 2
         self.state_count1 = 0  # current state of counter 1(binary)
         self.state_count2 = 0  # current state of counter 2(binary)
         self.total_wait_time = 0.0
@@ -58,24 +58,24 @@ class Simulation:
                 if numpy.random.choice([0,1]) == 0:  # choice takes a random number from given list if num is 0 he will go to teller 1 otherwise teller 2
                     self.state_count1 = 1  # 1 implies occupied
                     self.dep_1_service = gen_service_time_teller1()  # generates service time for the customer
-                    self.dep_sum_count1 += self.dep_1_service  # total service time provided by teller 1 increases by dep_1_service
+                    self.dep_sum_time1 += self.dep_1_service  # total service time provided by teller 1 increases by dep_1_service
                     self.time_leaving_count1 = self.clock + self.dep_1_service
                     self.time_arrival = self.clock + gen_int_arr()  # deciding next arrival
                 else:
                     self.state_count2 = 1  # 1 implies occupied1
                     self.dep_2_service = gen_service_time_teller2()  # generates service time for the customer
-                    self.dep_sum_count2 += self.dep_2_service  # total service time provided by teller 1 increases by dep_1_service
+                    self.dep_sum_time2 += self.dep_2_service  # total service time provided by teller 1 increases by dep_1_service
                     self.time_leaving_count2 = self.clock + self.dep_2_service
                     self.time_arrival = self.clock + gen_int_arr()  # deciding next arrival
             elif self.state_count1 == 0 and self.state_count2 == 1:
                 self.dep_1_service = gen_service_time_teller1()
-                self.dep_sum_count1 += self.dep_1_service
+                self.dep_sum_time1 += self.dep_1_service
                 self.time_leaving_count1 = self.clock + self.dep_1_service
                 self.time_arrival = self.clock + gen_int_arr()
                 self.state_count1 = 1
             else:
                 self.dep_2_service = gen_service_time_teller2()
-                self.dep_sum_count2 += self.dep_2_service
+                self.dep_sum_time2 += self.dep_2_service
                 self.time_leaving_count2 = self.clock + self.dep_2_service
                 self.time_arrival = self.clock + gen_int_arr()
                 self.state_count2 = 1
@@ -107,7 +107,7 @@ class Simulation:
         self.num_of_departures1 += 1
         if self.num_in_q > 0:
             self.dep_1_service = gen_service_time_teller1()
-            self.dep_sum_count1 += self.dep_1_service
+            self.dep_sum_time1 += self.dep_1_service
             self.time_leaving_count1 = self.clock + self.dep_1_service
             self.num_in_q -= 1
         else:
@@ -118,7 +118,7 @@ class Simulation:
         self.num_of_departures2 += 1
         if self.num_in_q > 0:
             self.dep_2_service = gen_service_time_teller2()
-            self.dep_sum_count2 += self.dep_2_service
+            self.dep_sum_time2 += self.dep_2_service
             self.time_leaving_count2 = self.clock + self.dep_2_service
             self.num_in_q -= 1
         else:
