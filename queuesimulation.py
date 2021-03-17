b4951e84e96534d6f4b66d46add8a3f74805630e
SakethGajawada <77500278+SakethGajawada@users.noreply.github.com>
2021-03-17 15:54:49 +0530

    Update queuesimulation.py

--- queuesimulation.py
+++ queuesimulation.py
@@ -1,4 +1,7 @@
 import numpy
+from tkinter import *
+import time
+
 #import GUI_backendinterface as gbi
 
 
@@ -124,3 +127,54 @@ class Queue:
         else:
             self.time_leaving_count2 = float('inf')
             self.state_count2 = 0
+
+
+q = Queue()
+
+customers_list =[]
+reciption = Tk()
+reciption.title("Registration")
+
+#backgroung image
+bg = PhotoImage(file = "download.png")
+label1 = Label( reciption, image = bg)
+label1.place(x = 0, y = 0,relwidth=1, relheight=1)
+
+#Clock
+def clock():
+  hour = time.strftime("%I")
+  minute =time.strftime("%M")
+  second =time.strftime("%S")
+  am_pm = time.strftime("%p")
+  lbl.config(text = hour +":" + minute +":"+second + " "+am_pm)
+  lbl.after(1000,clock)
+lbl = Label(reciption,text = "",font = ("Helvetica",48),fg = "red",bg = "black")
+lbl.place(x=0, y=0)
+clock()
+
+#entry
+def add_customer():
+  name_var = customers_name.get()
+  customers_list.append(name_var)
+  q.time_routines()
+  for_q = Label(reciption, text=str(q.num_in_q))
+  for_q.pack()
+  name_entry.delete(0, END)
+
+
+
+for_entry = Label(reciption, text="For entry")
+for_entry.place(x=400, y=200)
+
+customers_name = StringVar()
+name_entry = Entry(reciption, textvariable=customers_name)
+name_entry.place(x=400, y=220)
+
+join = Button(text="JOIN", command=add_customer, bg="black", fg="white")
+join.place(x=400, y=250)
+
+
+
+reciption.mainloop()
+print(customers_list)
+print(q.num_in_q)
