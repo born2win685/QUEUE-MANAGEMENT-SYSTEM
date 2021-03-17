72f1f6d8fb3fe95e83485abdfaf7de8fc91e98b8
markzuccu175 <pranav.vajreshwari@iiitb.org>
2021-03-17 13:13:59 +0530

    test

--- exportdata.py
+++ exportdata.py
@@ -5,8 +5,7 @@ import queuesimulation as qs
 
 s = qs.Queue()
 # making a pandas dataframe to store simulated data
-df = pd.DataFrame(columns=['Average interarrival time', 'Average service time teller1', 'Average service time teller 2',
-                           'Utilization teller 1', 'Utilization teller 2', 'People who had to wait in line',
+df = pd.DataFrame(columns=['Average interarrival time', 'Average service time teller1', 'Average service time teller 2','People who had to wait in line',
                            'Total average wait time', 'Lost Customers'])
 
 for i in range(10):
@@ -19,7 +18,7 @@ for i in range(10):
 
     a = pd.Series(
         [s.clock / s.no_of_arrivals, s.dep_sum_time1 / s.num_of_departures1, s.dep_sum_time2 / s.num_of_departures2,
-         s.dep_sum_time1 / s.clock, s.dep_sum_time2 / s.clock, s.total_cust_in_q, s.total_wait_time, s.lost_customers],
+         s.total_cust_in_q, s.total_wait_time, s.lost_customers],
         index=df.columns)
     df = df.append(a, ignore_index=True)
 
